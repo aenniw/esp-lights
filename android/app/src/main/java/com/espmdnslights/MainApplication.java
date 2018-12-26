be9decaefa123b76566bcf2c06f0ff88d8e552bd
Martin Mihálek <aenniw@gmail.com>
2018-12-26 13:22:40 +0100

    Eject expo to react-native
    - ejected expo
    - updated travis to build debug apk
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

---
+++ android/app/src/main/java/com/espmdnslights/MainApplication.java
@@ -0,0 +1,57 @@
+package com.espmdnslights;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
+import com.balthazargronon.RCTZeroconf.ZeroconfReactPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.asList(
+          new MainReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
+            new ZeroconfReactPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+
+    @Nullable
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
